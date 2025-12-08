4c3702ffe1918e8a8ea9e3fb241fa872ce3fa62c
Gyan Kushawaha <gyankushwaha77@gmail.com>
2025-12-08 19:17:59 +0530

    Updated API url for production

--- frontend/src/hooks/useTransaction.js
+++ frontend/src/hooks/useTransaction.js
@@ -32,7 +32,7 @@ const useTransactions = () => {
                 endDate: filters.endDate
             };
 
-            const response = await axios.get('http://localhost:5000/api/transactions', { params });
+            const response = await axios.get('https://truestate-sales-management-backend.onrender.com/api/transactions', { params });
             setData(response.data.data);
             setMeta(response.data.meta);
         } catch (error) {
